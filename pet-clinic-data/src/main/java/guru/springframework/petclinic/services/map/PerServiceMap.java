36e75e1caf8fb5114efe9ab8d6d6f0472b1553c6
Hongfu Sun <woshittshf@gmail.com>
2021-04-30 15:05:59 +0800

    adding Spring Config. closes #21

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/PerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/PerServiceMap.java
@@ -2,9 +2,11 @@ package guru.springframework.petclinic.services.map;
 
 import guru.springframework.petclinic.model.Pet;
 import guru.springframework.petclinic.services.PetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PerServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
