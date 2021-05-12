67c927d191c9245f2b8e2c707aee4989e0fed059
Hongfu Sun <woshittshf@gmail.com>
2021-05-12 10:57:31 +0800

    added PetTypeService, closes #28

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/PetServiceMap.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
-public class PerServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
     public Set<Pet> findAll() {
