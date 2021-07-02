26bd3bb62aace860f2d2934bf85b1aa1dc84e6c0
Hongfu Sun <woshittshf@gmail.com>
2021-07-02 17:24:27 +0800

    encounter Error resolving template problem in lecture 216

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/OwnerService.java
@@ -2,7 +2,11 @@ package guru.springframework.petclinic.services;
 
 import guru.springframework.petclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
